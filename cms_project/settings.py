438295227a621c7ada329971f90b13c9f25a7079
H0lwin <shayanqasmy88@gmail.com>
2025-11-27 13:45:42 +0330

    v8.0.2

--- cms_project/settings.py
+++ cms_project/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [str(BASE_DIR / 'static')]
-STATIC_ROOT = str(BASE_DIR / 'staticfiles') if not os.environ.get('STATIC_ROOT') else os.environ.get('STATIC_ROOT')
+STATIC_ROOT = os.path.join(str(BASE_DIR), 'staticfiles')
+STATICFILES_DIRS = []
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
