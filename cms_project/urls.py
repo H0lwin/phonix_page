90ae245650af71fd6768173db7900658f06ffe43
H0lwin <shayanqasmy88@gmail.com>
2025-12-02 16:42:52 +0330

    endddd

--- cms_project/urls.py
+++ cms_project/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from website import views
@@ -24,3 +26,6 @@ urlpatterns = [
     path('test/', views.test_view, name='test'),
     path('submit-testimonial/', views.SubmitTestimonialView.as_view(), name='submit_testimonial'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
